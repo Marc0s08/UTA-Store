32f82838e1f53f838e8b0286a7170b97c8348d42
Marcos Eduardo <marcoseduc2019@gmail.com>
2026-07-29 16:21:09 -0300

    Ajustar function Melhor Envio

--- src/pages/Admin/Settings.jsx
+++ src/pages/Admin/Settings.jsx
@@ -1,5 +1,6 @@
 import "./Settings.css";
 
+
 export default function Settings(){
 
 
@@ -7,32 +8,90 @@ export default function Settings(){
 
 
         const clientId =
+
         import.meta.env.VITE_MELHOR_ENVIO_CLIENT_ID;
 
 
 
-        const redirectUri =
-        window.location.origin +
-        "/oauth/callback";
+
+
+        if(!clientId){
+
+
+            alert(
+
+                "Client ID do Melhor Envio não configurado no Netlify"
+
+            );
+
+
+            return;
+
+
+        }
+
+
+
+
+
+
+        const redirectUri = encodeURIComponent(
+
+            window.location.origin +
+
+            "/oauth/callback"
+
+        );
+
+
+
 
 
 
         const url =
 
-        `https://melhorenvio.com.br/oauth/authorize` +
+        "https://melhorenvio.com.br/oauth/authorize" +
 
         `?client_id=${clientId}` +
 
         `&redirect_uri=${redirectUri}` +
 
-        `&response_type=code`;
+        "&response_type=code" +
+
+        "&scope=shipping-calculate";
+
+
+
+
+
+
+
+
+        console.log(
+
+            "URL OAuth Melhor Envio:",
+
+            url
+
+        );
+
+
+
+
+
 
 
 
         window.location.href = url;
 
 
+
     }
+
+
+
+
+
 
 
 
@@ -40,6 +99,9 @@ export default function Settings(){
 
 
         <main className="settings-page">
+
+
+
 
 
             <h1>
@@ -51,7 +113,13 @@ export default function Settings(){
 
 
 
+
+
+
             <section className="settings-card">
+
+
+
 
 
                 <h2>
@@ -62,12 +130,24 @@ export default function Settings(){
 
 
 
+
+
+
+
+
                 <p>
 
                     Conecte sua conta Melhor Envio
-                    para calcular fretes automaticamente.
+
+                    para calcular fretes automaticamente
+
+                    dentro da loja.
 
                 </p>
+
+
+
+
 
 
 
@@ -80,18 +160,29 @@ export default function Settings(){
 
                 >
 
+
                     🚚 Conectar Melhor Envio
 
+
                 </button>
+
+
+
+
 
 
 
             </section>
 
 
+
+
+
+
         </main>
 
 
-    )
+    );
+
 
 }
