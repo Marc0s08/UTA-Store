75cc5bb1002669e14b9167867147fcc8aed061d5
Marcos Eduardo <marcoseduc2019@gmail.com>
2026-08-04 16:37:31 -0300

    mudancas

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,31 +1,25 @@
 import "./Layout.css";
-
 import Navbar from "../Navbar/Navbar";
 import CartMessage from "../CartMessage/CartMessage";
+import { Outlet, useLocation } from "react-router-dom";
 
-import {
-    Outlet
-} from "react-router-dom";
+export default function Layout() {
+  const location = useLocation();
 
+  // Verifica se a rota atual começa com "/admin"
+  const isAdminRoute = location.pathname.startsWith("/admin");
 
-export default function Layout(){
+  return (
+    <>
+      {/* Esconde a Navbar principal se estiver no Admin */}
+      {!isAdminRoute && <Navbar />}
 
-    return (
+      {/* Esconde a mensagem do carrinho se estiver no Admin */}
+      {!isAdminRoute && <CartMessage />}
 
-        <>
-
-            <Navbar />
-
-            <CartMessage />
-
-            <main className="layout-content">
-
-                <Outlet />
-
-            </main>
-
-        </>
-
-    );
-
+      <main className="layout-content">
+        <Outlet />
+      </main>
+    </>
+  );
 }
